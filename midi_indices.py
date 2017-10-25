fcc45d95b65807b7f922932e04ca2953d8947a6e
Clockmender <clockmender@icloud.com>
2017-10-25 18:51:37 +0100

    MIDI Indices Node - latest version
    I have simplified it a little - makes it easier to maintain

--- midi_indices.py
+++ midi_indices.py
@@ -4,13 +4,13 @@ from bpy.props import *
 from ... base_types import AnimationNode
 
 class MidiIndex(bpy.types.PropertyGroup):
-    noteName = StringProperty() # e.g. C4, B5, ...
+    noteName = StringProperty()
     noteIndex = IntProperty()
 
 class MidiIndexNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_MidiIndexNode"
-    bl_label = "MIDI Evaluate Node, Vers 1.0"
-    bl_width_default = 400
+    bl_label = "MIDI Evaluation Node, Vers 1.0"
+    bl_width_default = 350
 
     notes = CollectionProperty(type = MidiIndex)
     cont_grp = StringProperty(name = "Controls Group")
@@ -19,10 +19,6 @@ class MidiIndexNode(bpy.types.Node, AnimationNode):
     message2 = StringProperty("")
 
     def create(self):
-        self.newInput("Object Group", "Channel Controls", "group1")
-        self.newInput("Object Group", "Key Meshes to Link", "group")
-        self.newOutput("Object List", "Control Objects", "objects1")
-        self.newOutput("Object List", "Key Objects", "objects")
         self.newOutput("Text List", "Notes Played in Channel", "notes")
         self.newOutput("Integer List", "Control to Key Index", "indices")
 
@@ -33,19 +29,17 @@ class MidiIndexNode(bpy.types.Node, AnimationNode):
         col = layout.column()
         col.scale_y = 1.5
         self.invokeFunction(col, "makeIndices", icon = "NEW",
-            text = "Create Control to Key Indices")
+            text = "Create Control to Key Index")
 
         if (self.message1 != ""):
             layout.label(self.message1, icon = "INFO")
         if (self.message2 != ""):
             layout.label(self.message2, icon = "INFO")
 
-    def execute(self, group1, group):
+    def execute(self):
         notes = [item.noteName for item in self.notes]
         indices = [item.noteIndex for item in self.notes]
-        control_Objs = list(getattr(group1, "objects", []))
-        keys_Objs = list(getattr(group, "objects", []))
-        return control_Objs, keys_Objs, notes, indices
+        return notes, indices
 
     def makeIndices(self):
         self.notes.clear()
@@ -56,6 +50,7 @@ class MidiIndexNode(bpy.types.Node, AnimationNode):
         else:
             cont_objs = cont_g.objects
             keys_objs = keys_g.objects
+
             numb_c = len(cont_objs)
             numb_k = len(keys_objs)
 
